9e78ff8ee57562a2c1395cf08afefb5b7807b816
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-05-24 13:35:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 //your JS code here. If required.
 const container = document.getElementByClassName("container")[0];
 function showElement(){
+	container.innerHTML = '';
 	const div = document.createElement("div");
 	div.id="line";
 	container.append(div);
+}
+
+function startAnimation(){
+	const line = document.getElementById("line");
+	line.className = "line";
+}
+
+function stopAnimation(){
+	const line = document.getElementById("line");
+	line.className = "";
 }
