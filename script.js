60c534c69b13f3696766947ebc48bd08e4f07e6d
pratik2820 <130150715+pratik2820@users.noreply.github.com>
2023-05-24 13:25:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const container = document.getElementByClassName("container")[0];
+function showElement(){
+	const div = document.createElement("div");
+	div.id="line";
+	container.append(div);
+}
